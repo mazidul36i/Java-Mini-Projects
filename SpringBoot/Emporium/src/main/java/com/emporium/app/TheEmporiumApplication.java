be59bd3b5ac281be061693054c6db54138ae4bdc
mazidul36i <islammazidul1369@gmail.com>
2023-01-02 14:14:33 +0530

    created new mini project

---
+++ SpringBoot/Emporium/src/main/java/com/emporium/app/TheEmporiumApplication.java
@@ -0,0 +1,13 @@
+package com.emporium.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheEmporiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheEmporiumApplication.class, args);
+	}
+
+}
---
+++ SpringBoot/Emporium/src/test/java/com/emporium/app/TheEmporiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emporium.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheEmporiumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
